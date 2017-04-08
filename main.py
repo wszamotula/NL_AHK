9343037882520de9ea0b3b848c5654d66c36d168
wszamotula <wes.szamotula@gmail.com>
2017-04-08 17:42:21 -0500

    More data sets
    Added data sets for launching websites/apps

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import sys
 import random
+import tkinter as tk
+from tkinter import filedialog
 from NER_classifier import *
 from nltk.chunk.util import conlltags2tree
 from nltk import pos_tag, word_tokenize
@@ -9,47 +11,65 @@ def main():
     """Given a english phrase, converts it into the appropriate AutoHotkey script"""
     # Determine what kind of script the request is asking for
     # Parse the relevant information out of the request and pass it to function
-    reader = read_gmb("key_remap.tags")
-    data = list(reader)
-    random.shuffle(data)
-    acc = 0
-    script_cor = 0
-    cross_val = False
 
-    if cross_val:
-        for i in range(len(data)):
-            test_sample = data[i]
-            training_samples = data[:]
-            del training_samples[i]
-            chunker = NamedEntityChunker(training_samples)
-            score = chunker.evaluate([conlltags2tree([(w, t, iob) for (w, t), iob in test_sample])])
-            print(score)
-            acc += score._tags_correct / score._tags_total
-            if score._tags_correct == score._tags_total:
-                script_cor += 1
-        print("\n" + "Overall tagging accuracy: {0:.2f}%".format(acc / len(data) * 100))
-        print("\n" + "Percentage of scripts correct: {0:.2f}%".format(script_cor / len(data) * 100))
+    cross_val("key_remap.tags")
+    cross_val("launch_website.tags")
+    cross_val("launch_app.tags")
 
-    else:
-        training_samples = data[:int(len(data) * 0.9)]
-        test_samples = data[int(len(data) * 0.9):]
-        chunker = NamedEntityChunker(training_samples)
-        for iobs in test_samples:
-            print(chunker.parse([(word, pos) for (word, pos), iob in iobs]))
+    # reader = read_gmb("key_remap.tags")
+    # data = list(reader)
+    # random.shuffle(data)
+    # training_samples = data[:int(len(data) * 0.9)]
+    # test_samples = data[int(len(data) * 0.9):]
+    # chunker = NamedEntityChunker(training_samples)
+    # for iobs in test_samples:
+    #     print(chunker.parse([(word, pos) for (word, pos), iob in iobs]))
 
     return
 
 
+def cross_val(tag_file):
+    print("\nRunning cross validation score on data set from " + tag_file +":")
+    reader = read_gmb(tag_file)
+    data = list(reader)
+    random.shuffle(data)
+    acc = 0
+    script_cor = 0
+    for i in range(len(data)):
+        test_sample = data[i]
+        training_samples = data[:]
+        del training_samples[i]
+        chunker = NamedEntityChunker(training_samples)
+        score = chunker.evaluate([conlltags2tree([(w, t, iob) for (w, t), iob in test_sample])])
+        # print(score)
+        acc += score._tags_correct / score._tags_total
+        if score._tags_correct == score._tags_total:
+            script_cor += 1
+    print("Overall tagging accuracy: {0:.2f}%".format(acc / len(data) * 100))
+    print("Percentage of scripts correct: {0:.2f}%".format(script_cor / len(data) * 100))
+    return
+
+
 def launch_site(key, modifier, site):
-    script = mod_to_char(modifier) + key + "::Run " + site
-    return script
+    if modifier:
+        print("The following script will launch the site {} when you press {} + {}".format(site, modifier, key))
+    else:
+        print("The following script will launch the site {} when you press {}".format(site, key))
+    print(mod_to_char(modifier) + key + "::Run " + site)
+    return
 
 
 def launch_app(key, modifier, app_name):
-    # Prompt user for full file path
-    file_path = ""
-    script = mod_to_char(modifier) + key + "::Run " + file_path
-    return script
+    print("Please select executable for application " + app_name)
+    root = tk.Tk()
+    root.withdraw()
+    file_path = filedialog.askopenfilename()
+    if modifier:
+        print("The following script will launch the app {} when you press {} + {}".format(file_path, modifier, key))
+    else:
+        print("The following script will launch the app {} when you press {}".format(file_path, key))
+    print(mod_to_char(modifier) + key + "::Run " + file_path)
+    return
 
 
 def rebind_key(old_key, modifier, new_key):
