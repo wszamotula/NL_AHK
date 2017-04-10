d879f1a3d13bed502a3f1fdf6a335b84c6851a37
wszamotula <wes.szamotula@gmail.com>
2017-04-10 17:26:15 -0500

    Interactive mode
    Added an interactive mode to the tool

--- main.py
+++ main.py
@@ -5,18 +5,47 @@ from tkinter import filedialog
 from NER_classifier import *
 from nltk.chunk.util import conlltags2tree
 from nltk import pos_tag, word_tokenize
-from script_type_classifier import classify_scripts
+import script_type_classifier
 
 def main():
     """Given a english phrase, converts it into the appropriate AutoHotkey script"""
     # Determine what kind of script the request is asking for
     # Parse the relevant information out of the request and pass it to function
+    evaluate = True
+    interactive = False
+    script_classifier = script_type_classifier.script_classifier("examples\script_type_examples.txt")
 
-    cross_val("examples\key_remap.tags")
-    cross_val("examples\launch_website.tags")
-    cross_val("examples\launch_app.tags")
+    if evaluate:
+        cross_val("examples\key_remap.tags")
+        cross_val("examples\launch_website.tags")
+        cross_val("examples\launch_app.tags")
+        script_classifier.evaluate_gnb()
 
-    classify_scripts("examples\script_type_examples.txt")
+    if interactive:
+        print("Currently supports key remapping, application launching, and website launching.")
+
+        key_chunker = learn_chunker("examples\key_remap.tags")
+        site_chunker = learn_chunker("examples\launch_website.tags")
+        app_chunker = learn_chunker("examples\launch_app.tags")
+        script_gnb = script_classifier.learn_gnb()
+
+        script_request = ""
+        while script_request != 'q':
+            script_request = input('Enter your script request (q to quit): ')
+            if script_request == 'q':
+                break
+            script_type = script_classifier.predict_script_type(script_gnb, script_request)
+            script_type = script_type[0].strip()
+
+            if script_type == 'key':
+                parsed_script = key_chunker.parse(pos_tag(word_tokenize(script_request)))
+                print(parsed_script)
+            elif script_type == 'site':
+                parsed_script = site_chunker.parse(pos_tag(word_tokenize(script_request)))
+                print(parsed_script)
+            elif script_type == 'app':
+                parsed_script = app_chunker.parse(pos_tag(word_tokenize(script_request)))
+                print(parsed_script)
 
     # reader = read_gmb("key_remap.tags")
     # data = list(reader)
@@ -29,6 +58,12 @@ def main():
 
     return
 
+
+def learn_chunker(tag_file):
+    reader = read_gmb(tag_file)
+    data = list(reader)
+    random.shuffle(data)
+    return NamedEntityChunker(data)
 
 def cross_val(tag_file):
     print("\nRunning cross validation score on data set from " + tag_file +":")
@@ -43,7 +78,6 @@ def cross_val(tag_file):
         del training_samples[i]
         chunker = NamedEntityChunker(training_samples)
         score = chunker.evaluate([conlltags2tree([(w, t, iob) for (w, t), iob in test_sample])])
-        # print(score)
         acc += score._tags_correct / score._tags_total
         if score._tags_correct == score._tags_total:
             script_cor += 1
--- script_type_classifier.py
+++ script_type_classifier.py
@@ -3,21 +3,80 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import cross_val_score
 
-def classify_scripts(file_name):
-    request_content, request_labels = read_file_content(file_name)
+class script_classifier():
 
-    vectorizer = CountVectorizer()
-    counts = vectorizer.fit_transform(request_content)
 
-    transformer = TfidfTransformer()
-    normalized_counts = transformer.fit_transform(counts)
+    def __init__(self, file_name):
+        self.request_content, self.request_labels = read_file_content(file_name)
 
-    gnb = GaussianNB()
-    scores = cross_val_score(gnb, normalized_counts.toarray(), request_labels, cv=10)
+        self.vectorizer = CountVectorizer()
+        self.counts = self.vectorizer.fit_transform(self.request_content)
 
-    print("\nEvalauated script identification with NB bag of words:")
-    print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
-    return
+        self.transformer = TfidfTransformer()
+        self.normalized_counts = self.transformer.fit_transform(self.counts)
+
+        return
+
+
+    def learn_gnb(self):
+        gnb = GaussianNB()
+        gnb.fit(self.normalized_counts.toarray(), self.request_labels)
+        return gnb
+
+
+    def evaluate_gnb(self):
+        gnb = GaussianNB()
+        scores = cross_val_score(gnb, self.normalized_counts.toarray(), self.request_labels, cv=10)
+
+        print("\nEvaluated script identification with NB bag of words:")
+        print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+        return
+
+
+    def predict_script_type(self, gnb, script_request):
+        counts = self.vectorizer.transform([script_request])
+        normalized_counts = self.transformer.transform(counts)
+        return gnb.predict(normalized_counts.toarray())
+
+
+# def learn_script_classifier(file_name, content="", labels=""):
+#     if content and labels:
+#         request_content = content
+#         request_labels = labels
+#     else:
+#         request_content, request_labels = read_file_content(file_name)
+#
+#     vectorizer = CountVectorizer()
+#     counts = vectorizer.fit_transform(request_content)
+#
+#     transformer = TfidfTransformer()
+#     normalized_counts = transformer.fit_transform(counts)
+#
+#     gnb = GaussianNB()
+#     gnb.fit(normalized_counts, request_labels)
+#     return gnb
+#
+#
+# def eval_script_classifier(file_name, content="", labels=""):
+#     if content and labels:
+#         request_content = content
+#         request_labels = labels
+#     else:
+#         request_content, request_labels = read_file_content(file_name)
+#
+#     vectorizer = CountVectorizer()
+#     counts = vectorizer.fit_transform(request_content)
+#
+#     transformer = TfidfTransformer()
+#     normalized_counts = transformer.fit_transform(counts)
+#
+#     gnb = GaussianNB()
+#     scores = cross_val_score(gnb, normalized_counts.toarray(), request_labels, cv=10)
+#
+#     print("\nEvalauated script identification with NB bag of words:")
+#     print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+#     return
+
 
 def read_file_content(file_name):
     request_content = []
